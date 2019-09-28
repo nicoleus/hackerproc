458ca7a3fa4e833a82a88b5dfccaa2f5d6510574
Nicoleus1 <55134353+nicoleus@users.noreply.github.com>
2019-09-28 15:59:12 +0700

    update.sh

---
+++ update.sh
@@ -0,0 +1,56 @@
+#
+#  -----------\\
+# ||         ||   
+# ||         ||   000
+# ||         ||   ____   _______    _______
+# ||         ||   |  |  |_______|  |       |
+# ||         ||   |  |  ||         |       |
+# ||         ||   |  |  ||________ | :D    |
+# ||         ||   |__|  |________| |_______|
+#          Hacking Tools by-Nicoleus Sitorus
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/hackerproc/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/hackerproc
+
+clear
+
+mkdir technicaldada
+
+clear
+
+cd hackerproc
+
+clear
+
+git clone https://github.com/nicoleus/hackerproc.py
+
+clear
+
+cd hackerproc
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
